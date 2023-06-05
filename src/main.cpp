7b53e5f1aec8f122da3966470ac07001ad516d64
Andras <andras.borsanyi09@gmail.com>
2023-06-05 18:59:53 +0200

    added spring generation

--- src/main.cpp
+++ src/main.cpp
@@ -19,25 +19,43 @@ void generate_atoms() {
 
 void generate_trimesh(int width, int depth, int height) {
     Atom* grid[width][depth][height];
-    float layer_height = sqrt(0.5);
+    float layer_height = sqrt(0.5); // height of a tetrahedron with an edge length of 1.0
 
-    int gridx, gridy, gridz;
-    for(float z = 0; z < height; z += 2*layer_height) {
-        for(float x = 0; x < width; x++) {
-            for(float y = 0; y < depth; y++) {
-                Atom atom_bottom(vec3(x, y, z), 1.0f);
-                Atom atom_top(vec3(x+0.5, y+0.5, z+layer_height), 1.0f);
+    // add atoms
+    for(int z = 0; z < height; z += 2) {
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < depth; y++) {
+                Atom atom_bottom(vec3(x, y, z*layer_height), 1.0f);
+                Atom atom_top(vec3(x+0.5, y+0.5, (z+1)*layer_height), 1.0f);
                 atom_list.push_back(atom_bottom);
                 atom_list.push_back(atom_top);
-
+                grid[x][y][z] = &atom_bottom;
+                grid[x][y][z+1] = &atom_top;
             }
         }
     }
 
+    // add springs
     for(int z = 0; z < height; z++) {
         for(int x = 0; x < width; x++) {
             for(int y = 0; y < depth; y++) {
-
+                if(x < width-1)
+                    spring_list.emplace_back(grid[x][y][z], grid[x+1][y][z], 1.0f);
+                if(y < height-1)
+                    spring_list.emplace_back(grid[x][y][z], grid[x][y+1][z], 1.0f);
+                if(z) {
+                    if(z % 2) {
+                        spring_list.emplace_back(grid[x][y][z], grid[x][y][z-1], 1.0f);
+                        spring_list.emplace_back(grid[x][y][z], grid[x+1][y][z-1], 1.0f);
+                        spring_list.emplace_back(grid[x][y][z], grid[x][y+1][z-1], 1.0f);
+                        spring_list.emplace_back(grid[x][y][z], grid[x+1][y+1][z-1], 1.0f);
+                    } else {
+                        spring_list.emplace_back(grid[x][y][z], grid[x][y][z-1], 1.0f);
+                        spring_list.emplace_back(grid[x+1][y][z], grid[x][y][z-1], 1.0f);
+                        spring_list.emplace_back(grid[x][y+1][z], grid[x][y][z-1], 1.0f);
+                        spring_list.emplace_back(grid[x+1][y+1][z], grid[x][y][z-1], 1.0f);
+                    }
+                }
             }
         }
     }
--- src/spring.h
+++ src/spring.h
@@ -9,11 +9,18 @@ class Spring {
         Atom* atom_b;
         float resting_length;
 
-        Spring(Atom* p_atom_a, Atom* p_atom_b, float p_resting_length) : 
+        Spring(Atom* p_atom_a, Atom* p_atom_b, float p_resting_length) :
             atom_a(p_atom_a), atom_b(p_atom_b), resting_length(p_resting_length) {
         }
 
         float calculate_length() {
-            return std::hypotf(std::hypotf(atom_a->pos.x - atom_b->pos.x, atom_a->pos.y - atom_b->pos.y), atom_a->pos.z - atom_b->pos.z);
+            // return std::hypotf(std::hypotf(atom_a->pos.x - atom_b->pos.x, atom_a->pos.y - atom_b->pos.y), atom_a->pos.z - atom_b->pos.z);
+            float xdiff = atom_a->pos.x - atom_b->pos.x;
+            float ydiff = atom_a->pos.y - atom_b->pos.y;
+            float zdiff = atom_a->pos.z - atom_b->pos.z;
+            std::cout << xdiff << " " << ydiff << " " << zdiff << " " << std::endl;
+            float d = sqrtf(xdiff*xdiff + ydiff*ydiff);
+            float l = sqrtf(d*d + zdiff*zdiff);
+            return l;
         }
 };
